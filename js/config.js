2a695609b1ec084efb63285ae2b8959a99a91c9b
sanskrutij-maker <sanskruti.jadhav@cumminscollege.in>
2025-05-01 00:50:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_3IwmRp5J5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '243fiolnm11o43r32mek2kao9f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5601jjrqnagqefq2t5e9tp825p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
